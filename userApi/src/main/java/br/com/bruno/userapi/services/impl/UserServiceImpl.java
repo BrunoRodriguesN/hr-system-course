569aac261487aed4dc732af2fc0536a94dd20ec5
BrunoRodriguesN <bruno_adeira@hotmail.com>
2022-05-21 11:16:31 -0300

    eureka service created

--- userApi/src/main/java/br/com/bruno/userapi/services/impl/UserServiceImpl.java
+++ userApi/src/main/java/br/com/bruno/userapi/services/impl/UserServiceImpl.java
@@ -5,21 +5,26 @@ import br.com.bruno.userapi.repositories.UserRepository;
 import br.com.bruno.userapi.services.UserService;
 import br.com.bruno.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private final UserRepository repository;
+    private final Environment env;
 
     @Override
     public User findById(Long id) {
+        log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + "port");
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 
     }
